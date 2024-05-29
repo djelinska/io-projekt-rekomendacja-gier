0d5841f7ef9cc03dd6a44716e2e4c0a442b69d60
Dominika Jelińska <dominikajelinska82@gmail.com>
2024-05-29 20:18:09 +0200

    Dopasowanie modelu i zwrócenie rekomendacji

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,16 @@
 import pickle
 
+from backend.models.tfidf_model import TFIDFModel
+
 with open('data/processed_games.pkl', 'rb') as f:
     processed_games_df = pickle.load(f)
 
-print(processed_games_df.head())
+model = TFIDFModel()
+model.train(processed_games_df['game_content'].tolist())
+
+title = "Baldur's Gate 3"
+game_content = processed_games_df.loc[processed_games_df['Title'] == title, 'game_content'].iloc[0]
+similar_games = model.find_similar_games(game_content, processed_games_df)
+
+for game in similar_games:
+    print(game)
---
+++ backend/models/tfidf_model.py
@@ -0,0 +1,19 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+
+
+class TFIDFModel:
+    def __init__(self):
+        self.tfidf_matrix = None
+        self.vectorizer = TfidfVectorizer()
+
+    def train(self, corpus):
+        self.tfidf_matrix = self.vectorizer.fit_transform(corpus)
+
+    def find_similar_games(self, query, data, n=11):
+        query_vector = self.vectorizer.transform([query])
+        cosine_similarities = linear_kernel(query_vector, self.tfidf_matrix).flatten()
+        related_indices = cosine_similarities.argsort()[:-n-1:-1]
+
+        similar_games = data.iloc[related_indices][['Title', 'Link', 'Game Description', 'Popular Tags']].to_dict(orient='records')
+        return similar_games
